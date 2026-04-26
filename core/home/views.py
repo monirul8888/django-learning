b90fd6e3a1de046128176d11280671ad75202e45
Monirul <monirul429588@gmail.com>
2026-04-26 14:54:34 +0600

    Added Peoples

--- core/home/views.py
+++ core/home/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
-    return render(request, "index.html")
+
+    peoples = [
+        {"name" : "Monirul", "dept": "CSE"},
+        {"name" : "Akib", "dept": "CSE"},
+        {"name" : "Antor", "dept": "EEE"}
+    ]
+
+
+    return render(request, "index.html", context={"peoples": peoples})
 
 def about(request):
     return HttpResponse("<h3> <b> BSc. in CSE </b> </h3>")
